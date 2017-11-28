c1b766c102eb43b38e56afb02045ff72a8821f46
Sean Miller <millersm@whitman.edu>
2017-11-27 21:18:19 -0800

    Add files via upload

--- cult.js
+++ cult.js
@@ -1,12 +1,21 @@
-var elements = document.getElementsByClassName('hot');
-if(elements.length >= 1){
-	alert("hello!");
+/*var elFilm = document.getElementById('userFilm');
+elFilm.addEventListener('submit', function(){
+	alert.log("Hello!");
+}, false);
+*/
+function find(){
+	var parEl = document.getElementById('yourFilm');
+	//var firstItem = parEl[0];
+	parEl.textContent = 'Hello';
 }
-var firstItem = elements[0];
-firstItem.textContent = 'hello';
-var cultscore = 5.8;
-var secondListNode = document.createElement('li');
-var newText = document.createTextNode('Star Wars' + ' ' + cultscore);
-secondListNode.appendChild(newText);
-var position = firstItem;
-position.appendChild(secondListNode);
+/*function filmsNearYou(){
+	var x = document.getElementById("userFilm");
+	var elements = document.getElementsByClassName('hot');
+	var firstItem = elements[0];
+	firstItem.textContent = 'hello';
+	var cultscore = 5.8;
+	var secondListNode = document.createElement('li');
+	var newText = document.createTextNode(x.value + ' ' + cultscore);
+	secondListNode.appendChild(newText);
+	firstItem.appendChild(secondListNode);
+}*/
