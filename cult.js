6c25302347baa6f9e5e0de729b190cee57949789
Sean Miller <millersm@whitman.edu>
2018-01-25 23:04:31 -0800

    Update cult.js

--- cult.js
+++ cult.js
@@ -1,14 +1,14 @@
-/*var elFilm = document.getElementById('userFilm');
+var elFilm = document.getElementById('userFilm');
 elFilm.addEventListener('submit', function(){
 	alert.log("Hello!");
 }, false);
-*/
+
 function find(){
 	var parEl = document.getElementById('yourFilm');
 	//var firstItem = parEl[0];
 	parEl.textContent = 'Hello';
 }
-/*function filmsNearYou(){
+function filmsNearYou(){
 	var x = document.getElementById("userFilm");
 	var elements = document.getElementsByClassName('hot');
 	var firstItem = elements[0];
@@ -18,4 +18,4 @@ function find(){
 	var newText = document.createTextNode(x.value + ' ' + cultscore);
 	secondListNode.appendChild(newText);
 	firstItem.appendChild(secondListNode);
-}*/
+}
